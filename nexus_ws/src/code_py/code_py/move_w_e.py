332b623013fa88dc56e1b42899057910c0b812de
leyl331 <lipaleyla9@gmail.com>
2025-07-31 17:22:02 -0400

    actualizacion de move_w_e.py

--- nexus_ws/src/code_py/code_py/move_w_e.py
+++ nexus_ws/src/code_py/code_py/move_w_e.py
@@ -4,48 +4,50 @@ from sensor_msgs.msg import JointState
 from ucb_interface.msg import Motor
 
 
-# Me falta:  Hacer que la velocidad sea en base al valor ingresado desde la WEB 
-#            ya que la vel de 0.1 es ctte miestras en la pajina sea > 0
-
-
 class MotorVisualizer(Node):
     def __init__(self):
-        super().__init__('motor_visualizer')
+        super().__init__('move_w_e')
 
-        
         self.create_subscription(Motor, '/motor_command', self.motor_callback, 10)
 
-        # Pub en los joint
+        # Pub joints de rviz
         self.joint_pub = self.create_publisher(JointState, '/joint_states', 10)
 
         # pos actual
         self.pos_izq = 0.0
         self.pos_der = 0.0
 
-        # vel 
+        # vel inicial
         self.vel_izq = 0.0
         self.vel_der = 0.0
 
-       
-        self.create_timer(0.1, self.actualizar_joints) # 0,1 ms
+        self.create_timer(0.1, self.actualizar_joints)
 
     def motor_callback(self, msg):
 
-        # vel 0.1 ( si desde la WEB - PWM > 0) 
-        self.vel_izq += 0.1 if msg.pwm_left > 0 else 0.0
-        self.vel_der += 0.1 if msg.pwm_right > 0 else 0.0
+        #  PWM (0-100) , vel (0.0 - 1.0)
+        escala = 0.01  
+
+        self.vel_izq = msg.pwm_left * escala
+        self.vel_der = msg.pwm_right * escala
+
+        if msg.pwm_left == 0.0:
+            self.vel_izq = 0.0
+        if msg.pwm_right == 0.0:
+            self.vel_der = 0.0
+
+        self.get_logger().info(
+            f"PWM -> Vel_izq: {self.vel_izq:.2f}, Vel_der: {self.vel_der:.2f}"
+        )
 
     def actualizar_joints(self):
+        # posicion de vel
+        self.pos_izq -= self.vel_izq
+        self.pos_der += self.vel_der
 
-        self.pos_izq -= self.vel_izq   # giro hacia adelante
-        self.pos_der += self.vel_der   # giro hacia adelante
-
-        # self.get_logger().info(f"pos_derecha: {self.pos_der}")
-
-        # msg a joints
+        # msg de JointState para publicar
         joint_msg = JointState()
         joint_msg.name = ['RuedaIzqierda_join', 'RuedaDerecha_join']
-
         joint_msg.position = [self.pos_izq, self.pos_der]
         joint_msg.header.stamp = self.get_clock().now().to_msg()
 
@@ -58,6 +60,7 @@ def main():
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
